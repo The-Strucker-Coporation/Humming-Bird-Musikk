7a7a9a640e5a3fe7563a59eddb6fa749db325c7b
Lennox <72359822+theyardmic3@users.noreply.github.com>
2024-10-20 23:44:43 +0000

    New Feature: Modified the Constants, Types and Utils to Match The Book Catalogue Data Structure.

--- types/index.ts
+++ types/index.ts
@@ -1,40 +1,27 @@
 import { MouseEventHandler } from "react";
 
-export interface CarProps {
-  city_mpg: number;
-  class: string;
-  combination_mpg: number;
-  cylinders: number;
-  displacement: number;
-  drive: string;
-  fuel_type: string;
-  highway_mpg: number;
-  make: string;
-  model: string;
-  transmission: string;
-  year: number;
+export interface BookProps {
+  title: string;
+  description: string;
+  grade: number;
+  category: "workbook" | "creative arts";
 }
 
 export interface FilterProps {
-  manufacturer?: string;
-  year?: number;
-  model?: string;
+  category?: "workbook" | "creative arts";
+  grade?: number;
   limit?: number;
-  fuel?: string;
 }
 
 export interface HomeProps {
   searchParams: FilterProps;
 }
 
-export interface CarCardProps {
-  model: string;
-  make: string;
-  mpg: number;
-  transmission: string;
-  year: number;
-  drive: string;
-  cityMPG: number;
+export interface BookCardProps {
+  title: string;
+  description: string;
+  grade: number;
+  category: "workbook" | "creative arts";
 }
 
 export interface CustomButtonProps {
@@ -62,7 +49,12 @@ export interface ShowMoreProps {
   isNext: boolean;
 }
 
-export interface SearchManuFacturerProps {
-  manufacturer: string;
-  setManuFacturer: (manufacturer: string) => void;
+export interface SearchCategoryProps {
+  category: "workbook" | "creative arts";
+  setCategory: (category: "workbook" | "creative arts") => void;
+}
+
+export interface SearchGradeProps {
+  grade: number;
+  setGrade: (grade: number) => void;
 }
